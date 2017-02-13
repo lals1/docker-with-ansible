5bf75e5bb113b0ec42dae00a2d535d9a434f688a
Shankar Lal <shankar.lal@live.com>
2017-02-13 14:17:06 +0000

    code added

--- code/app.py
+++ code/app.py
@@ -7,7 +7,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return ('Hello to my app on Docker, served by {}! ' 'There were {} visitors on this page.'
+    return ('Welcome to my app on Docker, served by {}! ' 'There were {} visitors on this page.'
              .format(os.environ['HOSTNAME'], redis.get('hits')))
 
 if __name__== "__main__":
